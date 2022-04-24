5a541b59e8b26db83096557154dceea8ad3d77d5
HALİL İBRAHİM KÜÇÜKDOĞRU <halilibrahimkucukdogru@posta.mu.edu.tr>
2022-04-24 22:34:36 +0300

    Add project files

---
+++ movie-tv-app.js
@@ -0,0 +1,212 @@
+let form = document.getElementById("search-form");
+const apiKey = "ebd943da4f3d062ae4451758267b1ca9";
+const imgSource = "https://image.tmdb.org/t/p/w500/";
+const defaultImgSource = `/images/no-image.png`;
+const baseUrl = "https://api.themoviedb.org/3/";
+const popularMovieUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US`;
+const popularTvShowUrl = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US`;
+const popularCelebUrl = `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US`;
+let input = document.getElementById("search-input");
+let navMovie = document.getElementById("movies");
+let navTVShow = document.getElementById("tvshows");
+let navCeleb = document.getElementById("celebs");
+let main = document.querySelector(".main");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    let query = input.value;
+    let url = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${query}&include_adult=false`
+    input.value = "";
+    await getData(url);
+});
+
+const getMoviesByGenre = async function () {
+    try {
+        main.innerHTML = "";
+        const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${this.id}&include_adult=false`)
+        response.data.results.forEach(element => {
+            let card = document.createElement("div");
+            card.classList.add("card");
+            let imgDiv = document.createElement("div");
+            let a = document.createElement("a");
+            let img = document.createElement("img");
+            let name = document.createElement("div");
+            let h3 = document.createElement("h3");
+            a.classList.add("detail-link");
+            a.id = element.id;
+            a.name = element.id;
+            a.setAttribute("data-type", "movie");
+            imgDiv.classList.add("img");
+            img.src = element.poster_path == null ? (defaultImgSource) : (imgSource + element.poster_path);
+            if (element.poster_path === undefined) {
+                img.src = element.profile_path == null ? (defaultImgSource) : (imgSource + element.profile_path);
+            }
+            name.classList.add("card-info");
+            h3.textContent = (element.title === undefined ? element.name : element.title);
+            name.style.textAlign = "center";
+            main.append(card)
+            card.append(imgDiv, name)
+            a.append(img);
+            imgDiv.append(a)
+            name.append(h3)
+        })
+
+        let detailLinks = document.querySelectorAll(".detail-link");
+        detailLinks.forEach(element => {
+
+            element.addEventListener("click", getDetail)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+const getMovieGenres = async () => {
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`);
+        let div = document.createElement("div");
+        let searchDiv = document.querySelector("div.search");
+        searchDiv.innerHTML = "";
+        searchDiv.append(div);
+        response.data.genres.forEach(element => {
+
+            let button = document.createElement("button");
+            button.textContent = element.name;
+            button.id = element.id;
+            button.classList.add("btn-genres");
+            div.append(button);
+        })
+        div.classList.add("genres");
+
+
+        let btnGenres = document.querySelectorAll(".btn-genres");
+        btnGenres.forEach(btn => {
+            btn.addEventListener("click", getMoviesByGenre);
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getDetail = async (e) => {
+    try {
+        main.innerHTML = "";
+        let id = e.target.parentNode.id;
+        let type = e.target.parentNode.getAttribute("data-type");
+        let element = document.querySelector(".detail-link");
+        let url = baseUrl + type + "/" + id + "?api_key=" + apiKey;
+        let response = await axios.get(url);
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.classList.add("card-flex");
+        let imgDiv = document.createElement("div");
+        let a = document.createElement("a");
+        let img = document.createElement("img");
+        let name = document.createElement("div");
+        let h3 = document.createElement("h3");
+        let overviewElement = document.createElement("p");
+        let popularityElement = document.createElement("p");
+        card.style.width = "80%";
+        h3.style.margin = "1rem";
+        imgDiv.classList.add("img");
+        img.src = response.data.poster_path == null ? (defaultImgSource) : (imgSource + response.data.poster_path);
+        if (response.data.poster_path === undefined) {
+            img.src = response.data.profile_path == null ? (defaultImgSource) : (imgSource + response.data.profile_path);
+        }
+        name.classList.add("card-info");
+        h3.textContent = "Name :" + (response.data.title === undefined ? response.data.name : response.data.title);
+        overviewElement.textContent = "Overview :" + (response.data.overview === undefined ? response.data.biography : response.data.overview);
+        popularityElement.textContent = "Popularity :" + response.data.popularity;
+        console.log(response.data);
+        name.style.textAlign = "center";
+        main.append(card)
+        card.append(imgDiv, name)
+        a.append(img);
+        imgDiv.append(a)
+        name.append(h3, overviewElement, popularityElement)
+
+
+
+
+    } catch (error) {
+        console.log(error);
+
+
+    }
+};
+
+
+const getData = async (url, type) => {
+    try {
+        if (type === "movie") {
+            getMovieGenres();
+        }
+        const response = await axios.get(url);
+        response.data.results.forEach(element => {
+            let card = document.createElement("div");
+            card.classList.add("card");
+            let imgDiv = document.createElement("div");
+            let a = document.createElement("a");
+            let img = document.createElement("img");
+            let name = document.createElement("div");
+            let h3 = document.createElement("h3");
+            a.classList.add("detail-link");
+            a.id = element.id;
+            a.name = element.id;
+            a.setAttribute("data-type", type);
+            imgDiv.classList.add("img");
+            img.src = element.poster_path == null ? (defaultImgSource) : (imgSource + element.poster_path);
+            if (element.poster_path === undefined) {
+                img.src = element.profile_path == null ? (defaultImgSource) : (imgSource + element.profile_path);
+            }
+            name.classList.add("card-info");
+            h3.textContent = (element.title === undefined ? element.name : element.title);
+            name.style.textAlign = "center";
+            main.append(card)
+            card.append(imgDiv, name)
+            a.append(img);
+            imgDiv.append(a)
+            name.append(h3)
+
+        })
+
+        let detailLinks = document.querySelectorAll(".detail-link");
+        detailLinks.forEach(element => {
+            element.addEventListener("click", getDetail)
+        });
+        let pagingDiv = document.createElement("div");
+        pagingDiv.classList.add("center");
+        document.body.append(pagingDiv);
+        for (let i = 1; i <= 10; i++) {
+
+            let pageLink = document.createElement("a");
+            pageLink.classList.add("paging-link")
+            pageLink.style.margin = "5px";
+            pageLink.innerText = i;            
+            pagingDiv.append(pageLink);
+        }
+
+        let pagingLinks = document.querySelectorAll(".paging-link");
+        pagingLinks.forEach(element => {            
+            let requestUrl = url +`&page=${element.innerText}`
+            element.addEventListener("click", getData(requestUrl, type))
+        })        
+    }
+
+    catch (error) {
+        console.log(error);
+    }
+};
+
+let callFunction = async (e, url, type) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    await getData(url, type);
+}
+
+navMovie.addEventListener("click", (e) => callFunction(e, popularMovieUrl, "movie"));
+navTVShow.addEventListener("click", (e) => callFunction(e, popularTvShowUrl, "tv"));
+navCeleb.addEventListener("click", (e) => callFunction(e, popularCelebUrl, "person"));
